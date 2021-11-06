4ac7fbac5d580821e93d1c1fc067176ac6d0bf05
MagicznySzymcio <theszymon450@gmail.com>
2021-11-06 20:10:09 +0100

    Update JoystickMovement.cs

--- Assets/JoystickMovement.cs
+++ Assets/JoystickMovement.cs
@@ -7,8 +7,9 @@ public class JoystickMovement : MonoBehaviour
     public Joystick joystick;
     public Rigidbody2D rigidbody2d;
     public float movementSpeed;
-    public float maxSpeed;
-    public float slowdownRate = 6f
+    public float maxSpeed = 3.0f;
+    public float maxSpeedSlowdown = 6.0f;
+    public float slowdownRate = 6.0f
         
         ;
     // Start is called before the first frame update
@@ -20,8 +21,35 @@ public class JoystickMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        SpeedControl();
+        JoystickInput();
         SlowDown();
+    }
+
+    void JoystickInput()
+    {
         rigidbody2d.AddRelativeForce(new Vector2(joystick.Horizontal * movementSpeed, joystick.Vertical * movementSpeed));
+    }
+
+    void SpeedControl()
+    {
+        if (rigidbody2d.velocity.x > maxSpeed)
+        {
+            rigidbody2d.AddForce(new Vector2(-maxSpeedSlowdown, 0));
+        }
+        else if (rigidbody2d.velocity.x < -maxSpeed)
+        {
+            rigidbody2d.AddForce(new Vector2(maxSpeedSlowdown, 0));
+        }
+
+        if (rigidbody2d.velocity.y > maxSpeed)
+        {
+            rigidbody2d.AddForce(new Vector2(0, -maxSpeedSlowdown));
+        }
+        else if (rigidbody2d.velocity.y < -maxSpeed)
+        {
+            rigidbody2d.AddForce(new Vector2(0, maxSpeedSlowdown));
+        }
     }
 
     void SlowDown()
