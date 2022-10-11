08a53f18f57d394723a96405c6875ca20cfab6a7
Szymon Palich <theszymon450@gmail.com>
2022-10-11 22:37:28 +0200

    Aktualizacja projektu do najnowszej wersji edytora

--- Assets/Scripts/Flare.cs
+++ Assets/Scripts/Flare.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.Rendering.Universal;
 
 public class Flare : MonoBehaviour
 {
